9ba9e915f1e917568662ce6d1117896e5d74fbc9
Rian Lucas <rianlucas1638@gmail.com>
2025-02-06 15:31:16 -0300

    Criando tabela no databse

---
+++ src/main/java/br/com/rianlucass/apiweb/model/Professor.java
@@ -0,0 +1,52 @@
+package br.com.rianlucass.apiweb.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+    private BigDecimal salario;
+    @Enumerated(EnumType.STRING)
+    private StatusProfessor statusProfessor;
+
+    public Professor(){ }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public StatusProfessor getStatusProfessor() {
+        return statusProfessor;
+    }
+
+    public void setStatusProfessor(StatusProfessor statusProfessor) {
+        this.statusProfessor = statusProfessor;
+    }
+}
---
+++ src/main/java/br/com/rianlucass/apiweb/model/StatusProfessor.java
@@ -0,0 +1,5 @@
+package br.com.rianlucass.apiweb.model;
+
+public enum StatusProfessor {
+    ATIVO, INATIVO ,APOSENTADO , AFASTADO
+}
